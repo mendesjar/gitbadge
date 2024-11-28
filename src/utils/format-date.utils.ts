c3c18787e13f53fbb15754e3f66b322c35a7934a
mendesjar <ga0301@outlook.com>
2024-11-28 10:15:43 -0300

    fix: format date setDate

--- src/utils/format-date.utils.ts
+++ src/utils/format-date.utils.ts
@@ -1,8 +1,5 @@
-let dataAtual = new Date();
-
-dataAtual.setDate(dataAtual.getDate() - 30);
-
 export function formatarData(data = new Date()) {
+  data.setDate(data.getDate() - 30);
   let ano = data.getFullYear();
   let mes = (data.getMonth() + 1).toString().padStart(2, "0");
   let dia = data.getDate().toString().padStart(2, "0");
