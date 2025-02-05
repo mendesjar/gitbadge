d820ec78817dcef5ad8a3686aeb5574c6e8b71d8
mendesjar <ga0301@outlook.com>
2025-02-05 17:35:01 -0300

    refactor: add interface user type state

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import {
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { useTheme } from "./components/theme-provider";
+import { IUser } from "./interfaces";
 
 const baseUrl = "https://api.github.com";
 
@@ -31,39 +32,7 @@ function App() {
       language: string | null;
     }[]
   >([]);
-  const [user, setUser] = useState<{
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    user_view_type: string;
-    site_admin: boolean;
-    name: string;
-    company: string | null;
-    blog: string;
-    location: string | null;
-    email: string | null;
-    hireable: string | null;
-    bio: string;
-    twitter_username: string | null;
-    public_repos: number;
-    public_gists: number;
-    followers: number;
-    following: number;
-  } | null>(null);
+  const [user, setUser] = useState<IUser | null>(null);
 
   async function getUser(username: string) {
     const apiData = await fetch(`${baseUrl}/users/${username}`);
