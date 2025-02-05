f141b67614558544c22a64b3c664059cf7d3ac16
mendesjar <ga0301@outlook.com>
2025-02-05 18:11:34 -0300

    feat: create interfaces commits and repositories

---
+++ src/interfaces/commits.interface.ts
@@ -0,0 +1,110 @@
+export interface Commits {
+  incomplete_results: boolean;
+  total_count: number;
+  items: Item[];
+}
+
+interface Item {
+  url: string;
+  sha: string;
+  node_id: string;
+  html_url: string;
+  comments_url: string;
+  commit: {
+    url: string;
+    author: {
+      date: string;
+      name: string;
+      email: string;
+    };
+    committer: {
+      date: string;
+      name: string;
+      email: string;
+    };
+    message: string;
+    tree: {
+      url: string;
+      sha: string;
+    };
+    comment_count: number;
+  };
+  author: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    user_view_type: string;
+    site_admin: boolean;
+  };
+  committer: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    user_view_type: string;
+    site_admin: boolean;
+  };
+  parents: [
+    {
+      url: string;
+      html_url: string;
+      sha: string;
+    }
+  ];
+  repository: {
+    id: number;
+    node_id: string;
+    name: string;
+    full_name: string;
+    private: boolean;
+    owner: {
+      login: string;
+      id: number;
+      node_id: string;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      user_view_type: string;
+      site_admin: boolean;
+    };
+  };
+  score: number;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,3 @@
 export type { User as IUser } from "./user.interface";
+export type { Commits as ICommits } from "./commits.interface";
+export type { Repositories as IRepositories } from "./repositories.interface";
